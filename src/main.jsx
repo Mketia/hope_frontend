ca48ee481f5cc7105887d8f7ca2bf4dde63b7d37
Mketia <k.mukakalis1@alustudent.com>
2025-03-28 12:31:20 +0200

    resolve error

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,35 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
-import App from './App.jsx';
-import Login from './pages/Login.jsx';
-import Reset from './pages/Reset.jsx';
-import Register from './pages/Register.jsx';  // Make sure this import is correct
-import About from './pages/about.jsx';
-import Donate from './pages/donate.jsx';
-import ScholarshipSection from './pages/ScholarshipSection.jsx';
-import Home from './pages/Home.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./pages/Login.jsx";
+import Reset from "./pages/Reset.jsx";
+import Register from "./pages/Register.jsx"; // Make sure this import is correct
+import About from "./pages/About.jsx";
+import Donate from "./pages/donate.jsx";
+import ScholarshipSection from "./pages/ScholarshipSection.jsx";
+import Home from "./pages/Home.jsx";
 import "../src/css/styles.css";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
 
 library.add(faInstagram, faTwitter, faYoutube);
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register />} />  {/* This is your register page */}
+        <Route path="/" element={<Register />} />{" "}
+        {/* This is your register page */}
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />  {/* Add this line */}
+        <Route path="/register" element={<Register />} /> {/* Add this line */}
         <Route path="/about" element={<About />} />
         <Route path="/donate" element={<Donate />} />
         <Route path="/reset" element={<Reset />} />
