df9a00c1b2be82aec4ce4a041148fc690d508386
Mketia <k.mukakalis1@alustudent.com>
2025-04-02 17:28:25 +0200

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "./App";
-import Login from "./pages/Login";
-import Reset from "./pages/Reset";
-import Register from "./pages/Register";
-import About from "./pages/About";
-import Donate from "./pages/Donate";
-import ScholarshipSection from "./pages/ScholarshipSection";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound"; // Create this if you haven't
+import App from "./App.jsx";
+import Login from "./pages/Login.jsx";
+import Reset from "./pages/Reset.jsx";
+import Register from "./pages/Register.jsx"; // Make sure this import is correct
+import About from "./pages/About.jsx";
+import Donate from "./pages/Donate.jsx";
+import ScholarshipSection from "./pages/ScholarshipSection.jsx";
+import Home from "./pages/Home.jsx";
 import "../src/css/styles.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faInstagram, faTwitter, faYoutube);
 
 const rootElement = document.getElementById("root");
 
@@ -19,16 +26,14 @@ createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="about" element={<About />} />
-          <Route path="donate" element={<Donate />} />
-          <Route path="reset" element={<Reset />} />
-          <Route path="scholarship" element={<ScholarshipSection />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
+        <Route path="/" element={<Register />} />{" "}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/donate" element={<Donate />} />
+        <Route path="/reset" element={<Reset />} />
+        <Route path="/scholarship" element={<ScholarshipSection />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
